c95d4f601c9d0adba2e8c0b837bf14b3f99f64f5
MohamedSaid2 <bouhssinamohamedsaid@gmail.com>
2026-08-14 23:07:37 +0200

    update server file

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const multer = require('multer');
-const cors = require('cors'); // Obligatoire pour autoriser GitHub Pages à contacter Render
+const cors = require('cors');
 const path = require('path');
+const fs = require('fs'); // 👈 Ajout du module File System
 
 const app = express();
+
+// 👈 Vérifie si le dossier 'uploads' existe, sinon il le crée
+const uploadDir = 'uploads';
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 // Autorise ton site GitHub Pages à envoyer des requêtes
 app.use(cors());
 
 // Dossier public pour lire les vidéos
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(uploadDir));
 
 // Configuration du stockage local sur le serveur
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'uploads/'),
+  destination: (req, file, cb) => cb(null, `${uploadDir}/`),
   filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
 });
 
@@ -23,7 +30,7 @@ const upload = multer({ storage });
 app.post('/upload', upload.single('video'), (req, res) => {
   if (!req.file) return res.status(400).json({ error: 'Aucun fichier reçu' });
   
-  // URL de la vidéo hébergée
+// URL de la vidéo hébergée
   const videoUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
   res.json({ success: true, url: videoUrl });
 });
